6b8772eed8390711e098444a6299fdf8ae37b296
Prashanth Swaminathan <Prashanth Swaminathan>
2022-07-18 13:32:18 +0200

    docs: Readme

--- functions/quiz/start.js
+++ functions/quiz/start.js
@@ -2,7 +2,6 @@ const airtable = require('airtable');
 
 exports.handler = function(context, event, callback) {
   let client = context.getTwilioClient();
-  // const client = require('twilio')("ACd563a914a66f7dd9669482a50fd3330c", "e7d0fa754b84db5cc8915f03d88a9907");
   const base = new airtable({apiKey: context.AIRTABLE_API_KEY}).base(context.AIRTABLE_BASE_ID)
   base(context.USER_TABLE_NAME).select().all()
     .then(users => {
