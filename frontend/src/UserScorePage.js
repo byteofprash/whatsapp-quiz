323759a777bcf60f78fc86d09abbfee44e237874
Prashanth Swaminathan <Prashanth Swaminathan>
2022-07-18 16:28:52 +0200

    Feat: Sending the correct answer for the frontend. Frontend shows emojis on the answer

--- frontend/src/UserScorePage.js
+++ frontend/src/UserScorePage.js
@@ -7,7 +7,7 @@ import {
   DataGridBody,
   DataGridCell,
 } from "@twilio-paste/core/data-grid";
-import { TableHeaderData, UserScoreData, UserScoreHeader } from "./constants";
+import { UserScoreHeader } from "./constants";
 import axios from 'axios'
 
 function UserScore({ userPhone }) {
@@ -16,7 +16,8 @@ function UserScore({ userPhone }) {
   let getData = async () => {
     setUserData([])
     const { data } = await axios.get(`https://waquiz-9106-dev.twil.io/quiz/userPerformance`, { params:{phone: userPhone[0]}});
-    setUserData(data.map((answer) => [answer.question, answer.rightAnswer, answer.yourAnswer]))
+    setUserData(data.map((answer) => [answer.question, answer.rightAnswer, answer.yourAnswer, answer.correct]))
+    console.log(data)
   };
   React.useEffect(() => {
     getData();
@@ -29,16 +30,24 @@ function UserScore({ userPhone }) {
           <DataGridHeader>{UserScoreHeader[0]}</DataGridHeader>
           <DataGridHeader>{UserScoreHeader[1]}</DataGridHeader>
           <DataGridHeader>{UserScoreHeader[2]}</DataGridHeader>
+          <DataGridHeader>{UserScoreHeader[3]}</DataGridHeader>
         </DataGridRow>
       </DataGridHead>
       <DataGridBody>
         {userData.map((row, rowIndex) => (
           <DataGridRow key={"row-" + rowIndex}>
-            {row.map((col, colIndex) => (
-              <DataGridCell key={"cell-" + rowIndex + "-" + colIndex}>
-                {col}
+              <DataGridCell key={"cell-" + rowIndex + "-0"}>
+                {userData[rowIndex][0]}
               </DataGridCell>
-            ))}
+              <DataGridCell key={"cell-" + rowIndex + "-0"}>
+                {userData[rowIndex][1]}
+              </DataGridCell>
+              <DataGridCell key={"cell-" + rowIndex + "-0"}>
+                {userData[rowIndex][2]}
+              </DataGridCell>
+              <DataGridCell key={"cell-" + rowIndex + "-0"}>
+                {userData[rowIndex][3]? "✅" : "❌"}
+              </DataGridCell>
           </DataGridRow>
         ))}
       </DataGridBody>
@@ -47,3 +56,13 @@ function UserScore({ userPhone }) {
 }
 
 export default UserScore;
+/*
+ {row.map((col, colIndex) => (
+              <DataGridCell key={"cell-" + rowIndex + "-" + colIndex}>
+                <div style={{color:row[1]!=row[2]? "red": ""}}>
+                    {col}
+                </div>
+              </DataGridCell>
+            ))}
+  
+ */
